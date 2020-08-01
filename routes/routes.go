a211414779c41f547cdf7a289a4bcab4e9d23933
Luke Whrit <lukewhrit@gmail.com>
2020-07-31 22:00:21 -0400

    chore: bump version to reflect latest release

--- routes/routes.go
+++ routes/routes.go
@@ -25,7 +25,7 @@ func Register(app *fiber.App) {
 		c.JSON(&fiber.Map{
 			"name":              "lynnx",
 			"description":       "a powerful, accessible, fast and lightweight URL shortener.",
-			"version":           "0.0.0",
+			"version":           "0.1.0",
 			"project_url":       "https://github.com/lukewhrit/lynnx/",
 			"documentation_url": "https://github.com/lukewhrit/lynnx/blob/master/spec.yml",
 			"author":            "Luke Whrit <lukewhrit@gmail.com> (https://lukewhrit.xyz)",
